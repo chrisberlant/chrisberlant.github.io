51f182b55e24e0212b0621cc53b70e29eb808a07
Chris Berlant <chris.berlant@gmail.com>
2024-06-03 23:08:45 +0200

    added minimum height to app

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import Header from './components/Header/Header';
 import Projects from './components/Projects/Projects';
 import HomePage from './components/HomePage/HomePage';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import MobileMenu from './components/MobileMenu/MobileMenu';
 import Tools from './components/Tools/Tools';
 import Footer from './components/Footer/Footer';
 
@@ -18,15 +17,11 @@ export default function App() {
 	return (
 		<Router>
 			<div
-				className={`app pt-8 pb-4 bg-background text-foreground lg:px-8 px-4`}
+				className={`app pt-8 pb-4 bg-background text-foreground lg:px-8 px-4 min-h-screen`}
 			>
 				<Header />
 				<main>
 					<AppRoutes />
-					{/* <Footer
-					currentPosition='Footer'
-					refs={[headerRef, skillsRef, projectsRef]}
-				/> */}
 				</main>
 				<Footer />
 			</div>
