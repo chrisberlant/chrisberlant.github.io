8939ffa4c358618e1cdbb2bb98cac20dd625ff0e
Chris Berlant <chris.berlant@gmail.com>
2024-07-31 17:55:11 +0200

    feat: GitHubActivity added loading and error handlers

--- src/components/HomePage/GitHubActivity/GitHubActivity.tsx
+++ src/components/HomePage/GitHubActivity/GitHubActivity.tsx
@@ -5,11 +5,18 @@ import { dateFormatting } from '@/lib/utils';
 import Blob from '@/components/Blob/Blob';
 import GitHubLogoWhite from '@assets/github-logo-white.svg';
 import GitHubLogoBlack from '@assets/github-logo-black.svg';
+import NotFoundAnimate from '@assets/not-found-animate.svg';
 import { ThemeProviderContext } from '../../ThemeProvider/ThemeProvider';
+import { LoaderCircleIcon } from 'lucide-react';
 
 export default function GitHubActivity() {
 	const { theme } = useContext(ThemeProviderContext);
-	const [commits, setCommits] = useState<CommitInfosType[]>([]);
+	// const [commits, setCommits] = useState<CommitInfosType[]>([]);
+	const [commits, setCommits] = useState({
+		data: null as CommitInfosType[] | null,
+		loading: true,
+		error: false,
+	});
 
 	useEffect(() => {
 		const abortController = new AbortController();
@@ -53,10 +60,14 @@ export default function GitHubActivity() {
 						: null
 				);
 
-				setCommits(uniqueCommits.slice(0, 5));
+				setCommits({
+					loading: false,
+					error: false,
+					data: uniqueCommits.slice(0, 5),
+				});
 			} catch (error: any) {
 				if (error.name !== 'AbortError')
-					console.error('Error fetching commits:', error);
+					setCommits({ ...commits, loading: false, error: true });
 			}
 		};
 
@@ -68,37 +79,65 @@ export default function GitHubActivity() {
 	}, []);
 
 	return (
-		<Card className='relative pb-5 pt-7 max-w-7xl'>
-			<Blob
-				svg={theme === 'light' ? GitHubLogoBlack : GitHubLogoWhite}
-				className='absolute transform -translate-x-1/2 -translate-y-1/2 opacity-70 -z-10 left-1/2 top-1/2 size-56'
-			/>
-			<h2 className='self-center px-4 mb-8 text-3xl font-bold text-center font-montserrat'>
+		<Card className='relative pb-5 sm:px-4 pt-7 max-w-7xl'>
+			<h2 className='px-4 mb-8 text-3xl font-bold text-center font-montserrat'>
 				Recent GitHub activity
 			</h2>
-			<ol className='flex flex-col gap-2 sm:pl-4 lg:pl-10 xl:pl-16'>
-				{commits?.map((commit) => (
-					<li key={commit.date}>
-						<a
-							className='text-lg font-semibold'
-							href={`https://github.com/${commit.title}`}
-							target='_blank'
-						>
-							{commit.title} - {dateFormatting(commit.date)}
-						</a>
-						<ol className='mt-1 text-sm font-robotoMono'>
-							{commit.messages.map((message) => (
-								<li
-									key={message}
-									className='mt-1 py-0.5 px-1.5 w-fit bg-muted/60 rounded-xl'
+
+			{commits.loading && (
+				<LoaderCircleIcon size={60} className='mb-4 animate-spin' />
+			)}
+
+			{commits.error && (
+				<div className='flex flex-col items-center max-w-xl'>
+					<img
+						src={NotFoundAnimate}
+						alt=''
+						className='lg:max-w-56 max-w-36'
+					/>
+					<span className='px-8 pt-2 text-center font-robotoMono'>
+						The GitHub server could not be joined or an error
+						happened while fetching the data
+					</span>
+				</div>
+			)}
+
+			{commits.data && (
+				<>
+					<Blob
+						svg={
+							theme === 'light'
+								? GitHubLogoBlack
+								: GitHubLogoWhite
+						}
+						className='absolute transform -translate-x-1/2 -translate-y-1/2 opacity-70 -z-10 left-1/2 top-1/2 size-56'
+					/>
+					<ol className='flex flex-col gap-2 sm:pl-4 lg:pl-10 xl:pl-16'>
+						{commits.data.map((commit) => (
+							<li key={commit.date}>
+								<a
+									className='text-lg font-semibold'
+									href={`https://github.com/${commit.title}`}
+									target='_blank'
 								>
-									{message}
-								</li>
-							))}
-						</ol>
-					</li>
-				))}
-			</ol>
+									{commit.title} -{' '}
+									{dateFormatting(commit.date)}
+								</a>
+								<ol className='mt-1 text-sm font-robotoMono'>
+									{commit.messages.map((message) => (
+										<li
+											key={message}
+											className='mt-1 py-0.5 px-1.5 w-fit bg-muted/60 rounded-xl'
+										>
+											{message}
+										</li>
+									))}
+								</ol>
+							</li>
+						))}
+					</ol>
+				</>
+			)}
 		</Card>
 	);
 }
