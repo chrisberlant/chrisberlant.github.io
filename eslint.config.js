e80576a9d716faf4f2a173000496152a705b1daa
Chris Berlant <chris.berlant@gmail.com>
2024-12-06 18:14:01 +0100

    fix: downgrade react and react-dom to 18.3.1

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,18 @@
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
 import eslintPluginAstro from 'eslint-plugin-astro';
 
 export default [
 	// add more generic rule sets here, such as:
 	// js.configs.recommended,
+	...pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	...pluginReact.configs.flat.recommended,
 	...eslintPluginAstro.configs.recommended,
 	{
 		files: ['src/**/*.{astro,tsx}'],
+		plugins: ['react-hooks'],
 		rules: {
 			'react-hooks/rules-of-hooks': 'error',
 			// override/add rules settings here, such as:
